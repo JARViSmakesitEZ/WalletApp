a7d75eef07664df2e891a26eac3af863a817047c
JARViSmakesitEZ <ghoshgourav9211@gmail.com>
2024-02-07 14:47:09 +0530

    added the addstudent endpoint

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,7 @@ app.post("/register", (req, res)); // Student registration
 // Admin-specific Endpoints
 app.post("/admin/update-balance", (req, res)); // Add or deduct balance from student account
 app.get("/admin/student/:id/account-details", (req, res)); // View student account details
+app.post("/admin/student/add", (req, res)); // add new student to the database
 
 // Student-specific Endpoints
 app.get("/student/account-details", (req, res)); // View student account details
